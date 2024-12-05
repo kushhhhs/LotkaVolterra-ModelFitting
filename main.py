dbb10e039ecde06a8fa778e6c23db45c8377adc3
kushhhhs <kushnavas@gmail.com>
2024-12-05 11:30:59 +0100

    Making sure modules work

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from src import load_data, simulate_model, plot_diffs
+from src import load_data, simulate_model
+from src.utils import plot_diffs
 
 t_obs, x_obs, y_obs = load_data('data/predator-prey-data.csv')
 
@@ -10,4 +11,4 @@ init_guess = (0.1, 0.02, 0.3, 0.1)# Initial guesses for the model parameters
 
 sim_data = simulate_model(init_guess, init_cond, t_obs)
 
-plot_diffs()
+plot_diffs(t_obs,x_obs, y_obs, sim_data[0], sim_data[1]) 
