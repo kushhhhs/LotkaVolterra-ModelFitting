ec84ac7f6d1de62dcf86fe84bf6836e8dc6334c8
kushhhhs <kushnavas@gmail.com>
2024-12-05 10:36:29 +0100

    Added Place Holder Functions for local and global optimization

--- src/optimization.py
+++ src/optimization.py
@@ -14,10 +14,17 @@ def compute_error(params: tuple, t_obs: np.ndarray, init_cond: list, x_obs: np.n
     simulate = simulate_model(params, t_obs, init_cond)  
     x_error = np.mean((simulate[0] - x_obs)**2)
     y_error = np.mean((simulate[1] - y_obs)**2)
-    return x_error, y_error
+    return (x_error + y_error)
 
-def local_optimization():
+def local_optimization(init_guess, bounds, t_obs, init_cond, x_obs, y_obs):
+    '''
+    Function to perform local optimization havent figured this out yet
+    '''
+
     return 0
 
 def global_optimization():
+    '''
+    Global optimization (Simulated annealing to find out optimized parameters)
+    '''
     return 0
