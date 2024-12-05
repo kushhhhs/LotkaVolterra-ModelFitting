f398abbfb8a2d58316bf07528b8325f2c9de08cc
kushhhhs <kushnavas@gmail.com>
2024-12-05 11:17:44 +0100

    Place holder main function

---
+++ main.py
@@ -0,0 +1,13 @@
+from src import load_data, simulate_model, plot_diffs
+
+t_obs, x_obs, y_obs = load_data('data/predator-prey-data.csv')
+
+init_cond=[x_obs[0], y_obs[0]]
+
+print(init_cond)
+
+init_guess = (0.1, 0.02, 0.3, 0.1)# Initial guesses for the model parameters 
+
+sim_data = simulate_model(init_guess, init_cond, t_obs)
+
+plot_diffs()
