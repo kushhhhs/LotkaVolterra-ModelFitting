7d3c7c344f023b652918bf76568af764f7b6e214
kushhhhs <kushnavas@gmail.com>
2024-12-14 09:43:12 +0100

    Added function to reduce from a combined time series

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src import load_data, simulate_model
 from src.utils import plot_diffs
-from src.optimization import local_optimization_RMSE, local_optimization_MAPE, global_optimization_RMSE, global_optimization_MAPE
-
+from src.optimization import local_optimization_RMSE, local_optimization_MAPE, global_optimization_RMSE, global_optimization_MAPE,compute_combined_rmse
+import numpy as np 
 t_obs, x_obs, y_obs = load_data('data/predator-prey-data.csv')
 
 
@@ -25,4 +25,26 @@ plot_diffs(t_obs, x_obs, y_obs, sim_data_global_RMSE[0], sim_data_global_RMSE[1]
 plot_diffs(t_obs, x_obs, y_obs, sim_data_local_MAPE[0], sim_data_local_MAPE[1])
 plot_diffs(t_obs, x_obs, y_obs, sim_data_global_MAPE[0], sim_data_global_MAPE[1])
 
+def combined_removal(t_obs, x_obs, y_obs, init_cond, bounds, tol):
+    removed_points = 0
+    i = 0
+    while i < len(t_obs) :
+        # Removing the i-th time point and corresponding prey observation
+        t_reduced = t_obs[i:]
+        y_reduced = y_obs[i:]
+        x_reduced = x_obs[i:]
+        removed_points += 1
+        # Fitting the parameters to the model using the reduced dataset
+        print(len(t_reduced))
+        optimized_params = global_optimization_RMSE(bounds, t_reduced, init_cond, x_reduced, y_reduced)
+        
+        combined_rmse = compute_combined_rmse(optimized_params, t_reduced, init_cond, x_reduced, y_reduced)
+        
+        if combined_rmse > tol:
+            print(f"Stopping: RMSE exceeded threshold of {tol}.")
+            break
+        
+        print(f"RMSE after removing {removed_points} time points = {combined_rmse:.4f}")
+        i += 1
+combined_removal(t_obs, x_obs, y_obs, init_cond, bounds, tol = 1)
 
--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,3 @@
 from .model import simulate_model
-from .optimization import local_optimization, global_optimization
+# from .optimization import local_optimization, global_optimization
 from .utils import load_data, plot_diffs, plot_sims
